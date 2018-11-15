a90bcc2d01590cb9aebc63a8faf424a109b5c396
Mayu14 <b4123504@edu.kit.ac.jp>
2018-11-16 00:05:59 +0900

    Sutherland's Lawを実装

--- flow_solver/EulerSolver2_2018/source/UnstructuredGrid/UnstFlux/Viscos/UGetLaminarViscosity.f90
+++ flow_solver/EulerSolver2_2018/source/UnstructuredGrid/UnstFlux/Viscos/UGetLaminarViscosity.f90
@@ -15,25 +15,29 @@ subroutine UGetLaminarViscosity(UConf, UG,UCC)
     use StructVar_Mod
     use LoopVar_Mod
     use FrequentOperation
-    use ConstantVar_Mod, Mu0 => ReferenceViscosity_Mu0  !&
-    !&   , Tref => ReferenceTemperature_Tref, SS => SutherlandTemperature_S, SC1 = SutherlandCoefficient1
+    use ConstantVar_Mod, gamma => SpecificOfHeatRatio
     implicit none
     type(Configulation), intent(in) :: UConf
     type(UnstructuredGrid), intent(in) :: UG
     type(CellCenter), intent(inout) :: UCC
+    double precision :: Mu0, STS, SC1, Mach2!, Tref
 
     if(UConf%UseSutherlandLaw == 0) then
-        UCC%LaminarViscosity = 1.0d0    !
+        UCC%LaminarViscosity = 1.0d0
+
     else
-        !call sutherland_law
+        Mu0 = ReferenceViscosity_Mu0
+        !Tref = ReferenceTemperature_Tref
+        STS = SutherlandTemperature_S
+        SC1 = SutherlandCoefficient1
+        Mach2 = MachNumber ** 2
+
+        do iCell = 1, UG%GI%RealCells
+            UCC%Temparature(iCell, 1, 1) = gamma * UCC%PrimitiveVariable(5, iCell, 1, 1) / UCC%PrimitiveVariable(1, iCell, 1, 1) * Mach2
+            UCC%LaminarViscosity(iCell, 1, 1) = SC1 * UCC%Temparature(iCell, 1, 1) ** 1.5d0 / (UCC%Temparature(iCell, 1, 1) + STS)
+        end do
+
     end if
 
-return
-contains
-    subroutine Sutherland_Law
-        implicit none
-
-        return
-    end subroutine Sutherland_Law
-
+    return
 end subroutine UGetLaminarViscosity
