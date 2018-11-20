319d2218d5875993eaa29f3564d23ee31bd7349a
Mayu14 <b4123504@edu.kit.ac.jp>
2018-11-21 01:43:57 +0900

    Add:convert MakeFile windows to linux(wsl)

---
+++ flow_solver/EulerSolver2_2018/convert_makefile_for_linux.py
@@ -0,0 +1,35 @@
+# -- coding: utf-8 --
+import os
+import re
+
+def main():
+    CFLAGS_ifort = "CFLAGS = -Wall -check uninit -check pointers -check bounds -check all -inline-level=0 -O0 -traceback -warn all -ftrapuv -debug full -zero -check uninit -check pointers -check bounds -check all -O0 -traceback -warn interfaces -warn all -ftrapuv -debug full -qopenmp -module $(OBJS_DIR) $(IDIR)\n"
+    LFLAGS_ifort = "LFLAGS = -qopenmp -s\n"
+
+    os.replace("Makefile", "Makefile_win")
+    flag = 0
+    with open(file="Makefile_win", mode="r") as fw:
+        with open(file="Makefile", mode="w") as fl:
+            for row in fw:
+                line = row
+
+                if line.startswith("EXE"):
+                    line = line.replace(".exe", "")
+                    flag = 1
+
+                if flag == 1:
+                    if line.startswith("FC"):
+                        line = "FC = mpif90\n"
+                    if line.startswith("LD"):
+                        line = "LD = mpif90\n"
+                    if line.startswith("CFLAGS"):
+                        line = CFLAGS_ifort
+
+                    if line.startswith("LFLAGS"):
+                        line = LFLAGS_ifort
+                        flag = 0
+                fl.write(line)
+
+
+if __name__ == '__main__':
+    main()
