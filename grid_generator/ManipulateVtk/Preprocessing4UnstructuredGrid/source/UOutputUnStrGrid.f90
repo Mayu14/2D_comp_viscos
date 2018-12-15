441add7cab8880d690a4827a06d9a509e5089759
Mayu14 <b4123504@edu.kit.ac.jp>
2018-12-16 00:28:20 +0900

    Fix:ファイルサイズ削減のためmayuファイル出力時のフォーマットを指定

--- grid_generator/ManipulateVtk/Preprocessing4UnstructuredGrid/source/UOutputUnStrGrid.f90
+++ grid_generator/ManipulateVtk/Preprocessing4UnstructuredGrid/source/UOutputUnStrGrid.f90
@@ -41,17 +41,17 @@ subroutine UOutputUnStrGrid(UG, cPath, cFileName, ExistBound)
 !三角形要素の幾何的関係
         write(1,*) "TriPoint ", UG%Tri%Total * 3
         do iCell=1, UG%Tri%Total
-            write(1,*) (UG%Tri%Point(iCell,iLoop),iLoop=1,3)
+            write(1,"(3(1x,i7))") (UG%Tri%Point(iCell,iLoop),iLoop=1,3)
         end do
 
         write(1,*) "TriEdge ", UG%Tri%Total * 3
         do iCell=1, UG%Tri%Total
-            write(1,*) (UG%Tri%Edge(iCell,iLoop),iLoop=1,3)
+            write(1,"(3(1x,i7))") (UG%Tri%Edge(iCell,iLoop),iLoop=1,3)
         end do
 
         write(1,*) "TriCell ", UG%Tri%Total * 3
         do iCell=1, UG%Tri%Total
-            write(1,*) (UG%Tri%Cell(iCell,iLoop),iLoop=1,3)
+            write(1,"(3(1x,i7))") (UG%Tri%Cell(iCell,iLoop),iLoop=1,3)
         end do
 
 !四辺形要素の幾何的関係
@@ -63,71 +63,71 @@ subroutine UOutputUnStrGrid(UG, cPath, cFileName, ExistBound)
 !線要素の幾何的関係
         write(1,*) "LinePoint ", UG%Line%Total * 2
         do iEdge=1, UG%Line%Total
-            write(1,*) (UG%Line%Point(iEdge,iLoop),iLoop=1,2)
+            write(1,"(2(1x,i7))") (UG%Line%Point(iEdge,iLoop),iLoop=1,2)
         end do
 
         write(1,*) "LineCell ", UG%Line%Total * 4
         do iEdge=1, UG%Line%Total
-            write(1,*) ((UG%Line%Cell(iEdge,iSide,iLocalEdge),iLocalEdge=1,2),iSide=1,2)
+            write(1,"(4(1x,i7))") ((UG%Line%Cell(iEdge,iSide,iLocalEdge),iLocalEdge=1,2),iSide=1,2)
         end do
 
 !仮想格子の幾何的関係
         write(1,*) "VCCell ", (UG%GI%AllCells - UG%GI%RealCells) * 2
         do iCell=UG%GI%RealCells+1, UG%GI%AllCells
-            write(1,*) (UG%VC%Cell(iCell,iLoop),iLoop=1,2)
+            write(1,"(2(1x,i7))") (UG%VC%Cell(iCell,iLoop),iLoop=1,2)
         end do
 
         write(1,*) "VCEdge ", UG%GI%AllCells - UG%GI%RealCells
         do iCell=UG%GI%RealCells+1, UG%GI%AllCells
-            write(1,*) UG%VC%Edge(iCell)
+            write(1,"(1x,i7)") UG%VC%Edge(iCell)
         end do
 
 !格子点の座標情報
         write(1,*) "PointC ", UG%GI%Points * 3
         do iPoint=1, UG%GI%Points
-            write(1,*) (UG%CD%Point(iPoint,iLoop),iLoop=1,3)
+            write(1,"(3(1x,f22.17))") (UG%CD%Point(iPoint,iLoop),iLoop=1,3)
         end do
 
 !格子境界中心の座標情報
         write(1,*) "EdgeC ", UG%GI%Edges * 3
         do iEdge=1,UG%GI%Edges
-            write(1,*) (UG%CD%Edge(iEdge,iLoop),iLoop=1,3)
+            write(1,"(3(1x,f22.17))") (UG%CD%Edge(iEdge,iLoop),iLoop=1,3)
         end do
 
 !格子要素中心の座標情報
         write(1,*) "CellC ", UG%GI%AllCells * 3
         do iCell=1,UG%GI%AllCells
-            write(1,*) (UG%CD%Cell(iCell,iLoop),iLoop=1,3)
+            write(1,"(3(1x,f22.17))") (UG%CD%Cell(iCell,iLoop),iLoop=1,3)
         end do
 
 !格子境界の面積
         write(1,*) "EdgeS ", UG%GI%Edges
         do iEdge=1,UG%GI%Edges
-            write(1,*) UG%GM%Area(iEdge)
+            write(1,"((2x,f22.14))") UG%GM%Area(iEdge)
         end do
 
 !格子境界の体積
         write(1,*) "CellV ", UG%GI%RealCells
         do iCell=1,UG%GI%RealCells
-            write(1,*) UG%GM%Volume(iCell)
+            write(1,"((2x,f22.14))") UG%GM%Volume(iCell)
         end do
 
 !セル界面における法線ベクトル
         write(1,*) "EdgeNormal ", UG%GI%Edges * 3
         do iEdge=1,UG%GI%Edges
-            write(1,*) (UG%GM%Normal(iEdge,iLoop),iLoop=1,3)
+            write(1,"(3(1x,f22.17))") (UG%GM%Normal(iEdge,iLoop),iLoop=1,3)
         end do
 
 !セル中心から界面中心までの距離ベクトル(中心基準)
         write(1,*) "Width ", UG%GI%RealCells
         do iCell=1,UG%GI%RealCells
-            write(1,*) ((UG%GM%Width(iCell,iLocalEdge,iLoop),iLoop=1,3),iLocalEdge=1,3)
+            write(1,"(3(1x,f22.17))") ((UG%GM%Width(iCell,iLocalEdge,iLoop),iLoop=1,3),iLocalEdge=1,3)
         end do
 
 !仮想セルに適用する境界条件
         write(1,*) "BoudnaryCondition_of_VirtualCell ", UG%GI%AllCells - UG%GI%RealCells
         do iCell=UG%GI%RealCells+1, UG%GI%AllCells
-            write(1,*) UG%GM%CellType(iCell,1,1)
+            write(1,"(1x,i7)") UG%GM%CellType(iCell,1,1)
         end do
 
 !Radius of Cell's Inscribed-Circle for compute CFL Condition
@@ -139,19 +139,19 @@ subroutine UOutputUnStrGrid(UG, cPath, cFileName, ExistBound)
 !内部に含む物体を構成する凸包のデータ
         write(1,*) "InternalObject ", UG%IO%iTotal
         do iPoint=1, UG%IO%iTotal
-            write(1,*) UG%IO%PointNum(iPoint)
+            write(1,"(1x,i7)") UG%IO%PointNum(iPoint)
         end do
 
 !MUSCLのLimiterで使用する各セルにおける界面までの平均距離
         write(1,*) "AverageWid ", UG%GI%RealCells
         do iCell=1,UG%GI%RealCells
-            write(1,*) UG%GM%AverageWidth(iCell)
+            write(1,"((2x,f22.14))") UG%GM%AverageWidth(iCell)
         end do
 
 !格子外周を構成する凸包のデータ
         write(1,*) "ConvexHull ", UG%CH%iTotal
         do iPoint=1, UG%CH%iTotal
-            write(1,*) UG%CH%PointNum(iPoint)
+            write(1,"(1x,i7)") UG%CH%PointNum(iPoint)
         end do
 
         if(ExistBound .eqv. .true.) then
@@ -171,13 +171,13 @@ subroutine UOutputUnStrGrid(UG, cPath, cFileName, ExistBound)
     ! 界面から最も近い物体表面
             write(1, *) "NearestSurfaceBoundaryEdgeNum4Edge ", UG%Line%Total
             do iEdge = 1, UG%Line%Total
-                write(1, *) UG%Line%Belongs2Wall(iEdge)
+                write(1, "(1x,i7)") UG%Line%Belongs2Wall(iEdge)
             end do
 
     ! 物体表面からの距離
             write(1, *) "DistanceFromObjectSurface4Edge ", UG%Line%Total
             do iEdge = 1, UG%Line%Total
-                write(1, *) UG%Line%Distance(iEdge)
+                write(1, "((2x,f22.14))") UG%Line%Distance(iEdge)
             end do
             write(1,*) ""
 
@@ -194,7 +194,7 @@ subroutine UOutputUnStrGrid(UG, cPath, cFileName, ExistBound)
             do iLoop = 1, UG%GM%BC%iWallTotal
                 write(1, *) UG%GM%BC%VW(iLoop)%iGlobalEdge, UG%GM%BC%VW(iLoop)%iNumberOfMemberEdge
                 do iEdge = 1,  UG%GM%BC%VW(iLoop)%iNumberOfMemberEdge
-                    write(1,*) UG%GM%BC%VW(iLoop)%iMemberEdge(iEdge)
+                    write(1,"(1x,i7)") UG%GM%BC%VW(iLoop)%iMemberEdge(iEdge)
                 end do
             end do
         end if
