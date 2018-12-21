1a3f8758a737bd0f2bb0c88f20b3505dec0d49bb
Mayu14 <b4123504@edu.kit.ac.jp>
2018-12-21 10:29:47 +0900

    Fix:medium用に調整，グラフの軸ラベル等パラメータ調整

--- post_processing.py
+++ post_processing.py
@@ -10,12 +10,13 @@ def main():
         i12digit = 4#i % 100
         i34digit = 11#5 * int(i / 100.0) + 11
         # char_angle = np.zeros((14, 2))
-        char_angle = np.zeros((9, 2))
+        char_angle = np.zeros((10, 2))
         # for angle in range(0, 42, 3):
         for angle in range(8, 28, 2):
-            deg = int(angle / 3)
+            # deg = int(angle / 3)
+            deg = int((angle - 8) / 2)
             # fname = "NACA" + str(i12digit).zfill(2) + str(i34digit).zfill(2) + "_" + str(angle).zfill(2) + "_AC.dat"
-            fname = "NACA0012_course_" + str(angle).zfill(2) + "_AC.dat"
+            fname = "NACA0012_medium_" + str(angle).zfill(2) + "_AC.dat"
             if os.path.exists(path + fname):
                 # print(fname + " " + str(os.path.exists(path + fname)))
                 drag = np.zeros(100, dtype = float)
@@ -33,7 +34,7 @@ def main():
             char_angle[deg, 1] = np.average(lift)
         
         # t = np.linspace(0, 39, 14)
-        t = np.linspace(8, 26, 9)
+        t = np.linspace(8, 28, 10)
         plt.plot(t, char_angle[:, 0])
         plt.plot(t, char_angle[:, 1])
         plt.show()
@@ -46,9 +47,9 @@ def cp_graph():
     for angle in range(8, 28, 2):
         deg = int(angle / 3)
         # fname = "NACA" + str(i12digit).zfill(2) + str(i34digit).zfill(2) + "_" + str(angle).zfill(2) + "_AC.dat"
-        fname = "CP_NACA0012_course_" + str(angle).zfill(2) + ".dat"
-        coord = np.zeros(500, dtype = float)
-        pressure = np.zeros(500, dtype = float)
+        fname = "CP_NACA0012_medium_" + str(angle).zfill(2) + ".dat"
+        coord = np.zeros(5000, dtype = float)
+        pressure = np.zeros(5000, dtype = float)
         if os.path.exists(path + fname):
             # print(fname + " " + str(os.path.exists(path + fname)))
             number = 0
@@ -59,10 +60,19 @@ def cp_graph():
                         break
                     attack = - angle / 180.0 * np.pi * 2.0  # 0.0
                     coord[number] = float(coord_and_pressure[0])
-                    pressure[number] = -float(coord_and_pressure[1])
+                    pressure[number] = float(coord_and_pressure[1])
                     number += 1
             
-            plt.plot(coord[:number], pressure[:number], "x")
+
+            fig = plt.figure()
+            ax = fig.add_subplot(1, 1, 1)
+            ax.set_title("NACA0012 Fine-Grid alpha = 10 deg")
+            ax.plot(coord[:number], pressure[:number], ".", label="Toyota. Re=5 mill. Ma=0.7")
+            ax.legend()
+            ax.set_xlabel("x/c")
+            ax.set_ylabel("Cp")
+            ax.grid()
+            ax.set_ylim(2.0, -6.0)
             plt.show()
             exit()
         
