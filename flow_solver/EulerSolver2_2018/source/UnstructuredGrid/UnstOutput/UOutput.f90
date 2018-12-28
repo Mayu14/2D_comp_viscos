13580aeba6a882f86822b9577983ea3c250a3af5
Mayu14 <b4123504@edu.kit.ac.jp>
2018-12-28 18:17:41 +0900

    Fix:非粘性条件下において，確保されていない配列の内容を書きだそうとする問題を修正

--- flow_solver/EulerSolver2_2018/source/UnstructuredGrid/UnstOutput/UOutput.f90
+++ flow_solver/EulerSolver2_2018/source/UnstructuredGrid/UnstOutput/UOutput.f90
@@ -111,19 +111,21 @@ subroutine UOutput(UConf,UG,UCC,iStep)
             write(1, "(f22.14)") dble(UG%Line%Distance(UG%Tri%Edge(iCell, 1))+UG%Line%Distance(UG%Tri%Edge(iCell, 2))+UG%Line%Distance(UG%Tri%Edge(iCell, 3))) / 3.0d0
         end do
 
-        write(1,"('SCALARS LaminarViscosity float')")
-        write(1,"('LOOKUP_TABLE default')")
+        if(invicid == .true.) then
+            write(1,"('SCALARS LaminarViscosity float')")
+            write(1,"('LOOKUP_TABLE default')")
 
-        do iCell=1, UG%GI%RealCells
-            write(1, "(f22.14)") UCC%LaminarViscosity(iCell,1,1)
-        end do
+            do iCell=1, UG%GI%RealCells
+                write(1, "(f22.14)") UCC%LaminarViscosity(iCell,1,1)
+            end do
 
-        write(1,"('SCALARS EddyViscosity float')")
-        write(1,"('LOOKUP_TABLE default')")
+            write(1,"('SCALARS EddyViscosity float')")
+            write(1,"('LOOKUP_TABLE default')")
 
-        do iCell=1, UG%GI%RealCells
-            write(1, "(f22.14)") UCC%EddyViscosity(iCell,1,1)
-        end do
+            do iCell=1, UG%GI%RealCells
+                write(1, "(f22.14)") UCC%EddyViscosity(iCell,1,1)
+            end do
+        end if
 
     close(1)
 return
