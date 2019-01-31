478195c2c78c85fd0b13375ed07705444233745b
Mayu14 <b4123504@edu.kit.ac.jp>
2019-01-31 16:52:50 +0900

    Fix:デフォルトを画面出力なしに変更

--- flow_solver/EulerSolver2_2018/source/ConstantVar_Mod.f90
+++ flow_solver/EulerSolver2_2018/source/ConstantVar_Mod.f90
@@ -28,7 +28,7 @@ implicit none
     integer :: CoreNumberOfCPU = 1
     integer :: GridNumber = 1!重合格子法にて使用する格子の数
     integer :: RetryFlag = 0 !音速が負になる等の問題が発生するとこのフラグが点灯し，再計算を実施する．フラグが点灯した状態で再度フラグ点灯しようとすると強制終了させる
-    integer :: DetailedReport = 3   ! 0で画面出力なし，1で開始・終了報告のみ，2でリトライの通知を追加，3で反復回数の通知を追加，4で残差通知を追加
+    integer :: DetailedReport = 0   ! 0で画面出力なし，1で開始・終了報告のみ，2でリトライの通知を追加，3で反復回数の通知を追加，4で残差通知を追加
 
     double precision :: dAttackAngle = 0.0d0
 
