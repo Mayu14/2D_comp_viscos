1e0e7314e87839cd89871b4d09b43f08ea7e74cc
Mayu14 <b4123504@edu.kit.ac.jp>
2019-02-01 16:50:42 +0900

    Fix:学習用csv吐き出しプログラムを追加

--- post_processing.py
+++ post_processing.py
@@ -1,11 +1,53 @@
 # coding: utf-8
 import numpy as np
-import pandas as pd
+import paramiko
 import matplotlib.pyplot as plt
 from matplotlib import rc
 import os
 
 def main():
+    password = input("input password of FMa037@afifep.ifs.tohoku.ac.jp")
+    path = "/work/A/FMa/FMa037/Case2/ResultC/"
+    save_path = "G:\\Toyota\\Data\\Compressible_Viscos\\training_data\\NACA4\\"
+    number = 6  # data number per wing
+    
+    max_angle = 39  # [deg]
+    min_angle = 0  # [deg]
+    delta_angle = 3  # [deg]
+    angle_variation = int((max_angle - min_angle) / delta_angle) + 1
+    
+    wing_valiation = 1620
+    total_data = wing_valiation * angle_variation
+    
+    save_data = np.zeros((total_data, number), dtype = float)
+    
+    type = 3.0
+    save_data[:, 0] = type
+    
+    with paramiko.SSHClient() as ssh:
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname = "afifep.ifs.tohoku.ac.jp", port = 22, username = "FMa037", password = password)
+        
+        count = 0
+        for i1 in range(1, 10):
+            for i2 in range(1, 10):
+                for i34 in range(12, 92, 4):
+                    naca4 = str(i1).zfill(1) + str(i2).zfill(1) + str(i34).zfill(2)
+                    for deg in range(min_angle, max_angle + delta_angle, delta_angle):
+                        save_data[count, 1] = float(naca4)
+                        save_data[count, 3] = float(deg)
+                        
+                        fname = "NACA" + naca4 + "_" + str(deg).zfill(2) + "_AC.dat"
+                        stdin, stdout, stderr = ssh.exec_command('cat ' + path + fname)
+                        cd_cl = stdout.readlines()[0].split()
+                        save_data[count, 4] = cd_cl[1]  # CL
+                        save_data[count, 5] = cd_cl[0]  # CD
+                        count += 1
+    
+    save_fname = save_path + "NACA4\\s1122_e9988_s4_a" + str(angle_variation).zfill(3) + ".csv"
+    np.savetxt(save_fname, save_data, delimiter = ",")
+    
+def cdcl_plot_test():
     path = "D:\\Toyota\\github\\2D_comp_viscos\\flow_solver\\EulerSolver2_2018\\ResultC\\test_NACA0012\\"
     for i in range(1800):
         i12digit = 0 #i % 100
@@ -62,15 +104,16 @@ def main():
         exit()
         
 
-def cp_graph():
+def cp_plot_test():
     path = "D:\\Toyota\\github\\2D_comp_viscos\\flow_solver\\EulerSolver2_2018\\ResultC\\test_NACA0012\\"
+    # fname = "CP_NACA" + str(i12digit).zfill(2) + str(i34digit).zfill(2) + "_" + str(angle).zfill(2) + ".dat"
+    fname = "CP_NACA0012_fine_rev2_" + str(angle).zfill(2) + ".dat"
     char_angle = np.zeros((9, 2))
     i12digit = 00
     i34digit = 12
     # for angle in range(-3, 42, 3):
     angle = 10
-    # fname = "CP_NACA" + str(i12digit).zfill(2) + str(i34digit).zfill(2) + "_" + str(angle).zfill(2) + ".dat"
-    fname = "CP_NACA0012_fine_rev2_" + str(angle).zfill(2) + ".dat"
+    
     coord = np.zeros(5000, dtype = float)
     pressure = np.zeros(5000, dtype = float)
     print(path + fname)
@@ -101,18 +144,11 @@ def cp_graph():
         ax.set_ylim(2.0, -6.0)
         plt.show()
             # exit()
-    
-def test():
-    i12list = [1,2,3,4,5,6,7,8,9]
-    for i in range(0, 1620):
-        i12 = int(float(i) / 20.0) + 11 + int(float(i) / 180.0)
-        # print(i, i1, int(float(i) / 180.0))
-        i34 = 4 * (i % 20) + 12
-        print(str(i12).zfill(2) + str(i34).zfill(2))
-        
+
 
 if __name__ == '__main__':
+    main()
     # rc('text', usetex=True)
-    # main()
-    # cp_graph()
-    test()
+    # cp_plot_test()
+    # cdcl_plot_test()
+    
