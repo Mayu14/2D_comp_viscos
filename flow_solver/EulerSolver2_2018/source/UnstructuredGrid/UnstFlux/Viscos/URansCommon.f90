3cd2364d86f0a2d1dab2118ad054693197ee9fff
Mayu14 <b4123504@edu.kit.ac.jp>
2019-02-05 15:59:14 +0900

    Fix:温度微分を音速で書きなおした

--- flow_solver/EulerSolver2_2018/source/UnstructuredGrid/UnstFlux/Viscos/URansCommon.f90
+++ flow_solver/EulerSolver2_2018/source/UnstructuredGrid/UnstFlux/Viscos/URansCommon.f90
@@ -63,10 +63,9 @@ contains
         double precision, allocatable :: average_velocity(:)
 
         integer :: iFrontLocalEdge, iBackLocalEdge
-        double precision :: dTdeta, dTdx, dTdy, length
+        double precision :: dc2dn, dc2dx, dc2dy, length
 
         invRe = 1.0d0 / ReynoldsNumber
-        invGmin1Mach2Pr = InverseGmin1 / (LaminarPrandtlNumber * MachNumber ** 2)
 
         allocate(average_velocity(2))
 
@@ -74,22 +73,25 @@ contains
             Viscosity = UCE%LaminarViscosity(iEdge, 1, 1) + UCE%EddyViscosity(iEdge, 1, 1)
             ThermalConductivity = UCE%LaminarViscosity(iEdge, 1, 1) / LaminarPrandtlNumber &
                               & + UCE%EddyViscosity(iEdge, 1, 1) / TurbulentPrandtlNumber
-            ! 界面の平均流束(2次精度中心差分)
+            ! 界面の平均流速(2次精度中心差分)
             average_velocity = 0.5d0 *(UCE%RebuildQunatity(2:3, 1, 1, 1, iEdge) + UCE%RebuildQunatity(2:3, 1, 1, 2, iEdge))
 
             call UCentralDifferencePrepareAroundFace(UG, iEdge, iFrontCell, iFrontLocalEdge, iBackCell, iBackLocalEdge, length)
             ! 界面に対し法線方向(eta方向)の温度微分
-            dTdeta = (UCC%Temparature(iFrontCell, 1, 1) - UCC%Temparature(iBackCell, 1, 1)) / length
+            dc2dn = gamma * (UCC%PrimitiveVariable(5,iFrontCell,1,1) / UCC%PrimitiveVariable(1,iFrontCell,1,1)  &
+                           - UCC%PrimitiveVariable(5,iBackCell,1,1) / UCC%PrimitiveVariable(1,iBackCell,1,1))
+
             ! 温度微分を座標変換により各座標方向に分配
-            dTdx = dTdeta * UG%GM%Normal(iEdge, 1)
-            dTdy = dTdeta * UG%GM%Normal(iEdge, 2)
+            dc2dx = dc2dn * UG%GM%Normal(iEdge, 1)
+            dc2dy = dc2dn * UG%GM%Normal(iEdge, 2)
+
             ! せん断応力の計算
             tau_xx = 2.0d0 / 3.0d0 * Viscosity * (2.0d0 * UCE%StrainRateTensor(1, 1, iEdge, 1, 1) - UCE%StrainRateTensor(2, 2, iEdge, 1, 1))
             tau_xy = Viscosity * (UCE%StrainRateTensor(1, 2, iEdge, 1, 1) + UCE%StrainRateTensor(2, 1, iEdge, 1, 1))
             tau_yy = 2.0d0 / 3.0d0 * Viscosity * (2.0d0 * UCE%StrainRateTensor(2, 2, iEdge, 1, 1) - UCE%StrainRateTensor(1, 1, iEdge, 1, 1))
 
-            beta_x = average_velocity(1) * tau_xx + average_velocity(2) * tau_xy + ThermalConductivity * invGmin1Mach2Pr * dTdx
-            beta_y = average_velocity(1) * tau_xy + average_velocity(2) * tau_yy + ThermalConductivity * invGmin1Mach2Pr * dTdy
+            beta_x = average_velocity(1) * tau_xx + average_velocity(2) * tau_xy + ThermalConductivity * gamma * InverseGmin1 * dc2dn
+            beta_y = average_velocity(1) * tau_xy + average_velocity(2) * tau_yy + ThermalConductivity * gamma * InverseGmin1 * dc2dy
 
             UCE%NormalFluxDiff(2, 1, 1, 1, iEdge) = UCE%NormalFluxDiff(2, 1, 1, 1, iEdge) &
                                                    & - invRe * (tau_xx * UG%GM%Normal(iEdge, 1) + tau_xy * UG%GM%Normal(iEdge, 2))
