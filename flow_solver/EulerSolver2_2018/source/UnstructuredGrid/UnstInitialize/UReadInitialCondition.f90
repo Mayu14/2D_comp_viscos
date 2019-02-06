04a8d725e8c4c5c527f93c8eaca78b7b6cc99dd2
Mayu14 <b4123504@edu.kit.ac.jp>
2019-02-06 17:40:24 +0900

    Fix:計算済みデータの読込部分にて自動生成ファイル名を使用するオプションを追加

--- flow_solver/EulerSolver2_2018/source/UnstructuredGrid/UnstInitialize/UReadInitialCondition.f90
+++ flow_solver/EulerSolver2_2018/source/UnstructuredGrid/UnstInitialize/UReadInitialCondition.f90
@@ -6,11 +6,22 @@ subroutine UReadInitialCondition(UConf,UG,UCC)
     type(Configulation), intent(in) :: UConf
     type(UnstructuredGrid), intent(in) :: UG
     type(CellCenter), intent(inout) :: UCC
-    character(len=64) :: cFileName
+    character(len=256) :: cFileName, cDirectory, cStep
     character :: cAnnotate,cAnnotate1
 
-    write(6,*) "Please input VTK-File name of Unstructured Grid for RESUME"
-    read(5,*) cFileName
+    if(UConf%SwitchProgram /= 7) then
+        write(6,*) "Please input VTK-File name of Unstructured Grid for RESUME"
+        read(5,*) cFileName
+    else
+        write(cStep,*) 0
+        if(UConf%CalcEnv == 0) then
+            cDirectory = "ResultU/" !UConf%SaveDirectiry   ! 研究室PC用
+            cFileName = trim(adjustl(cDirectory))//trim(adjustl(UConf%cFileName))//trim(adjustl("_"))//trim(adjustl(cStep))//"th.vtk"
+        else if(UConf%CalcEnv == 1) then
+            cDirectory = "/work/A/FMa/FMa037/Case2/ResultU/" ! 東北大スパコン用
+            cFileName = trim(adjustl(cDirectory))//trim(adjustl(UConf%cFileName))//trim(adjustl("_"))//trim(adjustl(cStep))//"th.vtk"
+        end if
+    end if
 
     open(unit = 1, file =trim(adjustl(cFileName)), status = 'old')
         write(6,*) "Find VTK-Data"
