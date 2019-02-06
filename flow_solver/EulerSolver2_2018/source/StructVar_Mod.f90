b16ee0a91aa14bdda481a050b12bb4d1059a2efc
Mayu14 <b4123504@edu.kit.ac.jp>
2019-02-06 14:20:54 +0900

    Fix:速度ノルムの定義を追加

--- flow_solver/EulerSolver2_2018/source/StructVar_Mod.f90
+++ flow_solver/EulerSolver2_2018/source/StructVar_Mod.f90
@@ -26,6 +26,7 @@ implicit none
 
         double precision, allocatable :: LaminarViscosity(:, :, :)
         double precision, allocatable :: EddyViscosity(:, :, :)
+        double precision, allocatable :: VelocityNorm(:, :, :)  !(i,j,k)
         double precision, allocatable :: Temparature(:, :, :)    ! (i, j, k)
         double precision, allocatable :: StrainRateTensor(:, :, :, :, :)   !ひずみ速度テンソル(uvw, xyz, i, j, k)
         double precision, allocatable :: AbsoluteVortisity(:, :, :) ! (i, j, k)
