3f8a1d3feaf0bc9d385820c75b70c29b57364ddf
Mayu14 <b4123504@edu.kit.ac.jp>
2019-02-07 02:43:15 +0900

    Fix:仮想セル用の処理を分離

--- flow_solver/EulerSolver2_2018/source/UnstructuredGrid/UnstFlux/Viscos/UGetLaminarViscosity_mk2.f90
+++ flow_solver/EulerSolver2_2018/source/UnstructuredGrid/UnstFlux/Viscos/UGetLaminarViscosity_mk2.f90
@@ -42,16 +42,22 @@ subroutine UGetLaminarViscosity_mk2(UConf, UG, UCC, UCE)
         SC1 = SutherlandTemperature_S / ReferenceTemperature_Tref
 
         !　セル中心で温度・粘性・速度ノルム求める
-        do iCell = 1, UG%GI%RealCells
+        do iCell = 1, UG%GI%AllCells
             UCC%Temparature(iCell, 1, 1) = gamma * UCC%PrimitiveVariable(5, iCell, 1, 1) / UCC%PrimitiveVariable(1, iCell, 1, 1) * Gmin1
             UCC%LaminarViscosity(iCell,1,1) = SutherlandViscosity(UCC%Temparature(iCell,1,1), Tinflow)
         end do
 
         do iEdge = 1, UG%GI%Edges
-            call GetLengthBetweenEdge(UG,iEdge,FrontLength,BackLength)
-            UCE%LaminarViscosity(iEdge,1,1) = (FrontLength * UCC%LaminarViscosity(iFrontCell,1,1) &
-                                         &  +  BackLength * UCC%LaminarViscosity(iBackCell,1,1))  &
-                                         &  / (FrontLength + BackLength)
+            iBackCell = UG%Line%Cell(iEdge,2,1)
+            if(iBackCell > UG%GI%RealCells) then
+                UCE%LaminarViscosity(iEdge,1,1) = 0.5d0 * (UCC%LaminarViscosity(iFrontCell,1,1) + UCC%LaminarViscosity(iBackCell,1,1))
+            else
+                call GetLengthBetweenEdge(UG,iEdge,iFrontCell,iBackCell,FrontLength,BackLength)
+                UCE%LaminarViscosity(iEdge,1,1) = (FrontLength * UCC%LaminarViscosity(iFrontCell,1,1) &
+                                             &  +  BackLength * UCC%LaminarViscosity(iBackCell,1,1))  &
+                                             &  / (FrontLength + BackLength)
+
+            end if
         end do
     end if
 
