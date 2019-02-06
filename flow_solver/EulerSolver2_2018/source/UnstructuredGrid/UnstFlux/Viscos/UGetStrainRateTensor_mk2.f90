80b867f4e0ae0aa27b4d29d4cf7b7738612ac429
Mayu14 <b4123504@edu.kit.ac.jp>
2019-02-07 02:44:29 +0900

    Fix:セル中心用の式で体積除算を追加・界面用の計算も追記

--- flow_solver/EulerSolver2_2018/source/UnstructuredGrid/UnstFlux/Viscos/UGetStrainRateTensor_mk2.f90
+++ flow_solver/EulerSolver2_2018/source/UnstructuredGrid/UnstFlux/Viscos/UGetStrainRateTensor_mk2.f90
@@ -21,20 +21,22 @@ subroutine UGetStrainRateTensor_mk2(UConf, UG, UCC, UCE)
     type(UnstructuredGrid), intent(in) :: UG
     type(CellCenter), intent(inout) :: UCC
     type(CellEdge), intent(in) :: UCE
-    integer :: iAdjC1, iAdjC2, iAdjC3
-    double precision :: MyLength, AdjLength
-    double precision :: totalLength
-    integer :: iVelDir, iDirectionW !面基準でのの向き(1 or 2)
+    double precision :: MyLength, AdjLength, totalLength
+    integer :: iDirectionW !面基準でのの向き(1 or 2)
     double precision :: DirectionN  !法線ベクトルの向き(Normalにこれを掛けると外向き法線になる)
     double precision, allocatable :: weight_ave_value(:)    !1~3:velocty, 4:Temparature; 距離の加重平均による速度と温度
     double precision :: MyTemparature, AdjTemparature
     double precision, allocatable :: dUdi(:), dVdi(:), dTdi(:)    ! 速度・温度の微分(1:x微分,2:y微分)
-
+    double precision :: FrontLength, BackLength
+    integer :: iFrontLocalEdge, iBackLocalEdge
+    double precision, allocatable :: velocity_dif(:)
 
     iDim = UG%GM%Dimension
     allocate(weight_ave_value(4))
     allocate(dUdi(2), dVdi(2), dTdi(2))
+    allocate(velocity_dif(2))
 
+    ! 実セルのみ
     do iCell = 1, UG%GI%RealCells
         dUdi = 0.0d0
         dVdi = 0.0d0
@@ -63,12 +65,38 @@ subroutine UGetStrainRateTensor_mk2(UConf, UG, UCC, UCE)
             dVdi(1:2) = dVdi(1:2) + weight_ave_value(2) * (DirectionN * UG%GM%Normal(iEdge,1:2)) * UG%GM%Area(iEdge)
             dTdi(1:2) = dTdi(1:2) + weight_ave_value(4) * (DirectionN * UG%GM%Normal(iEdge,1:2)) * UG%GM%Area(iEdge)
         end do
-        UCC%StrainRateTensor(1,1:2,iCell,1,1) = dUdi(1:2)
-        UCC%StrainRateTensor(2,1:2,iCell,1,1) = dUdi(1:2)
-        UCC%TemparatureGrad(1:2,iCell,1,1) = dTdi(1:2)
+        UCC%StrainRateTensor(1,1:2,iCell,1,1) = dUdi(1:2)　/ UG%GM%Volume(iCell)
+        UCC%StrainRateTensor(2,1:2,iCell,1,1) = dUdi(1:2)　/ UG%GM%Volume(iCell)
+        UCC%TemparatureGrad(1:2,iCell,1,1) = dTdi(1:2)　/ UG%GM%Volume(iCell)
 
     UCC%AbsoluteVortisity(iCell, 1, 1) = abs(UCC%StrainRateTensor(1, 2, iCell, 1, 1) &
                                          & - UCC%StrainRateTensor(2, 1, iCell, 1, 1))
+
+    end do
+
+
+    do iEdge = 1, UG%GI%Edges
+        iBackCell = UG%Line%Cell(iEdge,2,1)
+        if(iBackCell > UG%GI%RealCells) then
+            call UCentralDifferencePrepareAroundFace(UG, iEdge, iFrontCell, iFrontLocalEdge, iBackCell, iBackLocalEdge, totalLength)
+            velocity_dif(1:2) = UCE%RebuildQunatity(2:3, 1, 1, 1, iEdge) - UCE%RebuildQunatity(2:3, 1, 1, 2, iEdge) ! 表 - 裏
+            UCE%StrainRateTensor(:, 1, iEdge, 1, 1) = velocity_dif / totalLength * UG%GM%Normal(iEdge, 1)    ! 1:u,v, 2:x,y, 3:Edge, 4:1, 5:1
+            UCE%StrainRateTensor(:, 2, iEdge, 1, 1) = velocity_dif / totalLength * UG%GM%Normal(iEdge, 2)
+
+        else
+            call GetLengthBetweenEdge(UG,iEdge,iFrontCell,iBackCell,FrontLength,BackLength)
+            UCE%StrainRateTensor(1:2,1:2,iEdge,1,1) = (FrontLength * UCC%StrainRateTensor(1:2,1:2,iFrontCell,1,1) &
+                                                 &   + BackLength * UCC%StrainRateTensor(1:2,1:2,iBackCell,1,1))  &
+                                                 &   / (FrontLength + BackLength)
+
+            UCE%TemparatureGrad(1:2,1,1,1,iEdge) = (FrontLength * UCC%TemparatureGrad(1:2,iFrontCell,1,1) &
+                                                 &   + BackLength * UCC%TemparatureGrad(1:2,iBackCell,1,1))  &
+                                                 &   / (FrontLength + BackLength)
+
+        end if
+
+        UCE%AbsoluteVortisity(iEdge, 1, 1) = sqrt((UCE%StrainRateTensor(2, 1, iEdge, 1, 1) &
+                                               & - UCE%StrainRateTensor(1, 2, iEdge, 1, 1))**2)   ! dv/dx - du/dy
 
     end do
 
