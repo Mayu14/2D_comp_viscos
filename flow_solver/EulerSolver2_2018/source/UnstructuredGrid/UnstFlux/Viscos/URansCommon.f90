94f513ab76d599d326b3f9b971fc85a356e6ac75
Mayu14 <b4123504@edu.kit.ac.jp>
2019-02-07 02:42:47 +0900

    Fix:温度微分式の変更に対応

--- flow_solver/EulerSolver2_2018/source/UnstructuredGrid/UnstFlux/Viscos/URansCommon.f90
+++ flow_solver/EulerSolver2_2018/source/UnstructuredGrid/UnstFlux/Viscos/URansCommon.f90
@@ -21,26 +21,12 @@ subroutine URansCommon(UConf, UG, UCC, UCE)
     type(UnstructuredGrid), intent(in) :: UG
     type(CellCenter), intent(inout) :: UCC
     type(CellEdge), intent(inout) :: UCE
-    logical :: debug = .true.
     ! RANS common
     ! Calc Strain Rate Tensor & AbsoluteVortisity
     call UGetStrainRateTensor_mk2(UConf, UG, UCC, UCE)
 
     ! Calc Laminar Viscosity from Sutherland's Law
     call UGetLaminarViscosity_mk2(UConf, UG, UCC, UCE)
-
-    if(debug == .true.) then
-        do iCell = 1, UG%GI%RealCells
-            UCC%StrainRateTensor(1,1,iCell,1,1) = (UCE%StrainRateTensor(1,1,UG%Tri%Edge(iCell,1),1,1) + UCE%StrainRateTensor(1,1,UG%Tri%Edge(iCell,2),1,1) + UCE%StrainRateTensor(1,1,UG%Tri%Edge(iCell,3),1,1)) / 3.0d0
-            UCC%StrainRateTensor(1,2,iCell,1,1) = (UCE%StrainRateTensor(1,2,UG%Tri%Edge(iCell,1),1,1) + UCE%StrainRateTensor(1,2,UG%Tri%Edge(iCell,2),1,1) + UCE%StrainRateTensor(1,2,UG%Tri%Edge(iCell,3),1,1)) / 3.0d0
-            UCC%StrainRateTensor(2,1,iCell,1,1) = (UCE%StrainRateTensor(2,1,UG%Tri%Edge(iCell,1),1,1) + UCE%StrainRateTensor(2,1,UG%Tri%Edge(iCell,2),1,1) + UCE%StrainRateTensor(2,1,UG%Tri%Edge(iCell,3),1,1)) / 3.0d0
-            UCC%StrainRateTensor(2,2,iCell,1,1) = (UCE%StrainRateTensor(2,2,UG%Tri%Edge(iCell,1),1,1) + UCE%StrainRateTensor(2,2,UG%Tri%Edge(iCell,2),1,1) + UCE%StrainRateTensor(2,2,UG%Tri%Edge(iCell,3),1,1)) / 3.0d0
-        end do
-
-        do iCell = 1, UG%GI%RealCells
-            UCC%AbsoluteVortisity(iCell,1,1) = (UCE%AbsoluteVortisity(UG%Tri%Edge(iCell,1),1,1)+UCE%AbsoluteVortisity(UG%Tri%Edge(iCell,2),1,1)+UCE%AbsoluteVortisity(UG%Tri%Edge(iCell,3),1,1))/3.0d0
-        end do
-    end if
 
     if(UConf%TurbulenceModel == 1) then
         call UBaldwinLomax_main(UConf, UG, UCC, UCE)
@@ -63,7 +49,7 @@ contains
         double precision, allocatable :: average_velocity(:)
 
         integer :: iFrontLocalEdge, iBackLocalEdge
-        double precision :: dc2dn, dc2dx, dc2dy, length
+        double precision :: FrontLength, BackLength
 
         invRe = 1.0d0 / ReynoldsNumber
 
@@ -73,25 +59,20 @@ contains
             Viscosity = UCE%LaminarViscosity(iEdge, 1, 1) + UCE%EddyViscosity(iEdge, 1, 1)
             ThermalConductivity = UCE%LaminarViscosity(iEdge, 1, 1) / LaminarPrandtlNumber &
                               & + UCE%EddyViscosity(iEdge, 1, 1) / TurbulentPrandtlNumber
+
             ! 界面の平均流速(2次精度中心差分)
-            average_velocity = 0.5d0 *(UCE%RebuildQunatity(2:3, 1, 1, 1, iEdge) + UCE%RebuildQunatity(2:3, 1, 1, 2, iEdge))
-
-            call UCentralDifferencePrepareAroundFace(UG, iEdge, iFrontCell, iFrontLocalEdge, iBackCell, iBackLocalEdge, length)
-            ! 界面に対し法線方向(eta方向)の温度微分
-            dc2dn = gamma * (UCC%PrimitiveVariable(5,iFrontCell,1,1) / UCC%PrimitiveVariable(1,iFrontCell,1,1)  &
-                           - UCC%PrimitiveVariable(5,iBackCell,1,1) / UCC%PrimitiveVariable(1,iBackCell,1,1))
-
-            ! 温度微分を座標変換により各座標方向に分配
-            dc2dx = dc2dn * UG%GM%Normal(iEdge, 1)
-            dc2dy = dc2dn * UG%GM%Normal(iEdge, 2)
+            call GetLengthBetweenEdge(UG,iEdge,iFrontCell,iBackCell,FrontLength,BackLength)
+            average_velocity = (FrontLength * UCE%RebuildQunatity(2:3, 1, 1, 1, iEdge) &
+                            & + BackLength * UCE%RebuildQunatity(2:3, 1, 1, 2, iEdge)) &
+                            & / (FrontLength + BackLength)
 
             ! せん断応力の計算
             tau_xx = 2.0d0 / 3.0d0 * Viscosity * (2.0d0 * UCE%StrainRateTensor(1, 1, iEdge, 1, 1) - UCE%StrainRateTensor(2, 2, iEdge, 1, 1))
             tau_xy = Viscosity * (UCE%StrainRateTensor(1, 2, iEdge, 1, 1) + UCE%StrainRateTensor(2, 1, iEdge, 1, 1))
             tau_yy = 2.0d0 / 3.0d0 * Viscosity * (2.0d0 * UCE%StrainRateTensor(2, 2, iEdge, 1, 1) - UCE%StrainRateTensor(1, 1, iEdge, 1, 1))
 
-            beta_x = average_velocity(1) * tau_xx + average_velocity(2) * tau_xy + ThermalConductivity * gamma * InverseGmin1 * dc2dn
-            beta_y = average_velocity(1) * tau_xy + average_velocity(2) * tau_yy + ThermalConductivity * gamma * InverseGmin1 * dc2dy
+            beta_x = average_velocity(1) * tau_xx + average_velocity(2) * tau_xy + ThermalConductivity * gamma * InverseGmin1 * UCE%TemparatureGrad(1,1,1,1,iEdge)
+            beta_y = average_velocity(1) * tau_xy + average_velocity(2) * tau_yy + ThermalConductivity * gamma * InverseGmin1 * UCE%TemparatureGrad(2,1,1,1,iEdge)
 
             UCE%NormalFluxDiff(2, 1, 1, 1, iEdge) = UCE%NormalFluxDiff(2, 1, 1, 1, iEdge) &
                                                    & - invRe * (tau_xx * UG%GM%Normal(iEdge, 1) + tau_xy * UG%GM%Normal(iEdge, 2))
