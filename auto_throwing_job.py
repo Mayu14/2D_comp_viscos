86a5a67ca499b6b736b996cff4a8a7a6b7e52e8d
Mayu14 <b4123504@edu.kit.ac.jp>
2019-05-19 00:19:19 +0900

    Add:自動でプログラム・Makefile・job_scriptを修正→コンパイル→ジョブ投入までする

---
+++ auto_throwing_job.py
@@ -0,0 +1,155 @@
+# coding: utf-8
+import os
+from job_manager import run_unix
+
+def prepare_directory(workingpath):
+    dir_list = ["ResultU", "ResultC", "ResultR", "Complete/vtk", "Complete/dat", "Resume", "dummy"]
+    for dir in dir_list:
+        if os.path.exists(workingpath + os.sep + dir) == False:
+            os.makedirs(workingpath + dir)
+
+
+def update_JPCalcCaseAutoFill_f90(case_list,
+                                  path = "/home/FMa/FMa037/2D_comp_viscos/flow_solver/EulerSolver2_2018/source/Routine4JobParallel/"):
+    """
+    fortranプログラムを書き換える(JPCalcCaseAutoFill_originというファイルを作っておく)
+    :param case_list:   計算条件に関するリスト
+    :param path:        プログラムのあるフォルダ
+    :return:            None
+    """
+    # path = "/mnt/d/Toyota/github/2D_comp_viscos/flow_solver/EulerSolver2_2018/source/Routine4JobParallel/"  # debug
+    
+    fname = path + "JPCalcCaseAutoFill"
+    with open(fname + "_origin", "r") as f:
+        main_program = f.read()
+
+    s1 = "    "
+    s2 = s1 + s1
+    s3 = s1 + s2
+    s4 = s2 + s2
+    with open(fname + ".f90", "w") as f:
+        f.write(main_program)
+        f.write(
+            s1 + "subroutine grid_change(UConf)\n" + s2 + "implicit none\n" + s2 + "type(Configulation), intent(inout) :: UConf\n")
+        for i in range(len(case_list)):
+            f.write(s3)
+            if i != 0:
+                f.write("else ")
+            f.write("if(Uconf%my_rank == " + str(i) + ") then\n")
+            f.write(s4 + 'Uconf%cGridName = trim(adjustl("' + case_list[i][0] + '"))\n')
+            f.write(s4 + 'Uconf%cFileName = trim(adjustl("' + case_list[i][1] + '"))\n')
+            f.write(s4 + 'UConf%cCaseName = trim(adjustl("' + case_list[i][2] + '"))\n')
+            f.write(s4 + 'UConf%dAttackAngle = ' + case_list[i][3] + '\n')
+            f.write(s4 + 'UConf%ResumeFileName = trim(adjustl("' + case_list[i][4] + '"))\n')
+        f.write(
+            s3 + "end if\n" + s2 + "return\n" + s1 + "end subroutine grid_change\nend subroutine JPCalcCaseAutoFill\n")
+
+def update_makefile(program_name, makefile_path = "/home/FMa/FMa037/2D_comp_viscos/flow_solver/EulerSolver2_2018/",
+                    debug = False, compiler = "mpiifort"):
+    """
+    Makefileを書き換える
+    :param program_name:    出力されるプログラムの名称
+    :param path:            Makefileのおいてあるパス
+    :param debug:           デバッグ用のコンパイルオプションにするかどうか
+    :param compiler:        どのコンパイラを使うか(mpiif90とかに書き換えるかも)
+    :return: None
+    """
+    # makefile_path = "/mnt/d/Toyota/github/2D_comp_viscos/flow_solver/EulerSolver2_2018/"  # debug
+    
+    body = "EXE = " + program_name + "\n"
+    body += "FC = " + compiler + "\nLD =  " + compiler + "\nIDIR =\n"
+    if debug:
+        body += "CFLAGS = -check uninit -check pointers -check bounds -qopenmp -stand f90 -fp-stack-check -O0 -traceback -warn all -ftrapuv -debug full\n"
+    else:
+        body += "CFLAGS = -qopenmp -O4\n"
+    
+    fname = makefile_path + "Makefile"
+    
+    with open(fname + "_header", "r") as f:
+        header = f.read()
+    
+    with open(fname + "_footer", "r") as f:
+        footer = f.read()
+    
+    with open(fname, "w") as f:
+        f.write(header)
+        f.write(body)
+        f.write(footer)
+    
+    # clock skew detected 対策
+    run_unix("sleep 5")
+
+def generate_qsub(fname, jobname, parallel, program, mpi = True, comargs = "", sync = "n"):
+    """
+    qsubスクリプトを生成する
+    :param fname: qsubスクリプト名
+    :param jobname: jobの名前
+    :param parallel: 並列数
+    :param program: プログラムの名前
+    :param mpi: mpi並列を使うかどうか
+    :param comargs: コマンドライン引数
+    :param sync: ジョブ終了までフォアグラウンドで動かすかどうか
+    :return:
+    """
+    
+    def set_jobclass_and_time(parallel, mpi = False):
+        if mpi:
+            time = "20:00:00"
+            if parallel < 961:
+                jobclass = "dma.LS"
+            elif parallel < 2881:
+                jobclass = "dma.LM"
+            else:
+                print("use-mpi and parallel > 2880")
+                print("please check the parallel number and parallel type")
+                exit()
+        
+        else:  # openmp
+            time = "99:00:00"
+            if parallel < 41:
+                jobclass = "dma.LS"
+            elif parallel < 81:
+                jobclass = "smb.A"
+            elif parallel < 161:
+                jobclass = "smb.B"
+            else:
+                print("use-openmp and parallel > 160")
+                print("please check the parallel number and parallel type")
+                exit()
+        return jobclass, time
+    
+    jobclass, time = set_jobclass_and_time(parallel, mpi)
+    header = "#!/bin/bash\n###UGEオプション####\n#$ -P GR06APR18\n#$ -cwd\n"
+    body = "#$ -jc " + jobclass + "\n#$ -N " + jobname + "\n#$ -l h_rt=" + time + "\n#$ -pe OpenMP " + str(
+        parallel) + "\n#$ -sync " + sync + "\n"
+    footer = ". /etc/profile.d/modules.sh\nmodule load intel/2018.2.046\n"
+    last = "./" + program + " " + comargs
+    with open(fname, "w") as f:
+        f.write(header)
+        f.write(body)
+        f.write(footer)
+        f.write(last)
+
+
+def auto_throwing_job(qsubname, jobname, parallel, case_list, program_name = "EulerSolver2", make_script="make.sh"):
+    # f90ファイル書き換え
+    update_JPCalcCaseAutoFill_f90(case_list)
+    # makefile書き換え&make
+    update_makefile(program_name, makefile_path = "", debug = False)
+    
+    run_unix("./" + make_script) # cd /home/FMa/FMa037/2D_comp_viscos/flow_solver/EulerSover2_2018/; make
+    # qsub書き換え・ジョブ投入
+    generate_qsub(qsubname, jobname, parallel, program_name)
+    run_unix("qsub " + qsubname)
+
+
+if __name__ == '__main__':
+    qsubname = "auto_qsub.sh"
+    jobname = "auto_job"
+    parallel = 10
+    case_list = [
+        ['/mnt/g/Toyota/Data/grid_vtk/valid/mayu/NACA0012.mayu', 'NACA0012__AoA30.0__Ma0.80__Re50000__case5', 'case5',
+         '30.0d0', '/mnt/d/Toyota/github/2D_comp_viscos/Running/NACA0012__AoA30.0__Ma0.80__Re50000__case5__Resume.vtk'],
+        ['/mnt/g/Toyota/Data/grid_vtk/valid/mayu/NACA21092.mayu', 'NACA21092__AoA23.5__Ma0.30__Re4000__case5', 'case5',
+         '23.5d0', '/mnt/d/Toyota/github/2D_comp_viscos/Running/NACA21092__AoA23.5__Ma0.30__Re4000__case5__Resume.vtk']]
+    auto_throwing_job(qsubname, jobname, parallel, case_list, program_name = "ES2")
