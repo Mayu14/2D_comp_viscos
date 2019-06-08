bfbc16a627c496987e4dc9faf33e47f539c50f1a
Mayu14 <b4123504@edu.kit.ac.jp>
2019-06-09 01:49:01 +0900

    Fix:internal_point廃止，start_points, deform追加，

--- grid_generator/mask_maker.py
+++ grid_generator/mask_maker.py
@@ -5,16 +5,16 @@ import matplotlib.pyplot as plt
 import sys
 
 class VPmask(object):
-    def __init__(self, x, y, canvas_size, internal_point=None, imglim=None, aspect_ratio=1.0, obj_margin=4,
+    def __init__(self, x, y, canvas_size, start_point="lb", imglim=None, deform="None", obj_margin=4,
                  outerframe_margin=3, line_width=0.5):
         """
         物体表面のx,yの座標の配列を入力し，rgb配列に変換する(x,yは一筆書きできるように整列済みであるとする)
         :param x:   (list as [ndarray x-coords obj1, ndarray x-coords obj2, ... ])    # object surface points coordinates of x
         :param y:   (list as [ndarray y-coords obj1, ndarray y-coords obj2, ... ])    # object surface points coordinates of y
         :param canvas_size: (list as [x_resolution, y_resolution])  # image canvas size (pixel)
-        :param internal_point:   (list as [[x_obj1, y_obj1], [x_obj2, y_obj2], ..., ]) # A point inside the closed curve that is the start point of the filling process   # 指定がないときはx, y方向の中心座標
+        :param start_point:   (character) # A start point of floodFill (start point must be outside of the object) "lb" > "leftbottom", "rt" > "righttop", "lt", "rb"
         :param imglim:    (list as [[x_min, x_max], [y_min, y_max]])    # upper and lower limit of drawing range (x & y-direction)  # 指定がないときは，長軸方向に合わせた正方形領域を選ぶ
-        :param aspect_ratio: (float) x/y of image (no deformation at defualt)
+        :param deform: (character or float) "None", "Fit", or x/y (aspect_ratio)
         :param obj_margin: (int) set margin from object to boundary (margin = obj_margin * number of pixels)
         :param outerframe_margin: (int) outer frame size of matplotlib's graph
         :param line_width: (float) thickness of curve representing object   (大きいと形状表現精度低下・小さすぎるとマスク生成失敗率上昇)
@@ -29,25 +29,17 @@ class VPmask(object):
         self.obj_margin = obj_margin
         self.outerframe_margin = outerframe_margin
 
+        # 閉曲線を変形する
+        self.deform_curve(deform)
+
         if type(imglim) == type(None):
             imglim = self.__get_imglim__()
 
         self.xlim = imglim[0]
         self.ylim = imglim[1]
 
+        self.__set_start_points__(start_point)
 
-        if type(internal_point) == type(None):
-            internal_point = self.__set_internal_points__(internal_point)
-        elif internal_point[0][0].dtype == np.zeros(1, dtype=float).dtype:
-            internal_point = self.__set_internal_points__(internal_point)
-        elif type(internal_point[0][0]) != np.zeros(1, dtype=int).dtype:
-            print("Error! variable 'internal_point' must be 'None', 'float-list', or  'int-list'")
-            print("force kill this process.")
-            exit()
-
-        self.internal_point = internal_point
-
-        self.aspect_ratio = aspect_ratio
         self.lw = line_width
         # 閉曲線x, yが一筆書きになるよう並べ替え処理を入れたい
 
@@ -55,7 +47,6 @@ class VPmask(object):
         self.img = self.get_curve_img()
 
         self.mask = self.get_mask(self.img)
-
 
     def __get_min_and_max__(self):
         min_x = np.inf  # 初期化
@@ -71,6 +62,18 @@ class VPmask(object):
         self.min = [min_x, min_y]
         self.max = [max_x, max_y]
 
+    def deform_curve(self, deform):
+        self.deform = False
+        if deform != "None":
+            self.deform = True
+            if deform == "Fit":
+                deform = (self.max[0] - self.min[0]) / (self.max[1] - self.min[1])  # x/y
+            center_y = 0.5 * (self.max[1] + self.min[1])
+
+            for i in range(self.object_number):
+                self.y[i] = deform * (self.y[i] - center_y) + center_y
+            self.aspect_ratio = deform
+            self.__get_min_and_max__()
 
     def __get_imglim__(self):
         min_x = self.min[0]
@@ -86,9 +89,11 @@ class VPmask(object):
             eps = dy / float(self.canvas_size[1])
 
         eps *= float(self.obj_margin)   # marginで指定されたpixel数だけmarginを確保する
-        lim = [(min(min_x, min_y) - eps), (max(max_x, max_y) + eps)]  # 長軸方向に余白を与える
-
-        return [lim, lim]    # 長軸方向基準の正方形領域とする
+        if self.deform:
+            return [[min_x - eps, max_x + eps], [min_y - eps, max_y + eps]]  # 長軸方向に余白を与える
+        else:
+            lim = [(min(min_x, min_y) - eps), (max(max_x, max_y) + eps)]  # 長軸方向に余白を与える
+            return [lim, lim]    # 長軸方向基準の正方形領域とする
 
     def __set_internal_points__(self, internal_point):
         int_pts = []
@@ -107,6 +112,33 @@ class VPmask(object):
                 int_pts.append(pix_xy)
 
         return int_pts
+
+    def __set_start_points__(self, start_points):
+        def left_or_bottom():
+            return 5
+
+        def right_or_top(right=True):
+            if right:
+                direction = 0
+            else:
+                direction = 1
+            return self.canvas_size[direction] - 5
+
+        if start_points == "lb":
+            xs = left_or_bottom()
+            ys = left_or_bottom()
+        elif start_points == "lt":
+            xs = left_or_bottom()
+            ys = right_or_top(right=False)
+        elif start_points == "rb":
+            xs = right_or_top()
+            ys = left_or_bottom()
+        else:
+            xs = right_or_top()
+            ys = right_or_top(right=False)
+
+        self.start_points = [xs, ys]
+
 
     def coords2pixel(self, pts_xy):
         """
@@ -134,14 +166,8 @@ class VPmask(object):
                        bottom=False, top=False, left=False, right=False,
                        labelbottom=False, labeltop=False, labelleft=False, labelright=False)
 
-        fig.canvas.draw()
-
-        # *** プロット画像の保存 ***
-        # plt.savefig("tak.png")
-        # *** 画像をOpenCV画像としてロード ***
-        # img = cv2.imread("tak.png")
         # matplotlibの出力結果をnumpy配列に変換
-
+        fig.canvas.draw()
         img = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
         return img.reshape(fig.canvas.get_width_height()[::-1] + (3,))
 
@@ -156,17 +182,11 @@ class VPmask(object):
         # *** 領域塗りつぶしと結果の表示
         # cv2.floodFill( img, mask, (120,120), (0,0,0))   # 1st:img, 2nd:mask 3rd:seed point 4th:new value
 
-        sz = img.shape[:2]
-
         # cv2.floodFill(self.img, mask, (int(sz[0]/2), int(sz[1]/2)), (0,0,0))
         # cv2.floodFill(self.img, mask, (self.internal_point[0][0], self.internal_point[0][1]), (0,0,0))
-        cv2.floodFill(img, mask, (2+3, 2+3), (0, 0, 0))    # 物体じゃない側を塗りつぶす
+        cv2.floodFill(img, mask, (self.start_points[0], self.start_points[1]), (0, 0, 0))    # 物体じゃない側を塗りつぶす
         # cv2.imshow("canny",mask)
         # self.img = 255 - self.img   # reverse
-        #"""
-        cv2.imshow("flood",self.img)
-        cv2.waitKey()
-        #"""
         xy_min = self.outerframe_margin
         x_max = (img.shape[0]) - xy_min
         y_max = (img.shape[1]) - xy_min
@@ -180,7 +200,10 @@ class VPmask(object):
         cv2.imshow("img", self.img)
         cv2.waitKey()
 
-    def save_img(self, fname):
+    def save_img(self, fname_without_extension, format="png"):
+        if self.deform:
+            fname_without_extension += "__" + str(self.aspect_ratio) + "__"
+        fname = fname_without_extension + "." + format
         cv2.imwrite(fname, self.img)
 
 def test_independent():
@@ -194,9 +217,7 @@ def test_independent():
         y.append(r*np.sin(tht))
 
 
-
-
-def test_naca(return_int_pts = True):
+def test_naca():
     from grid_generator.naca_4digit_test import Naca_4_digit
     attack_angle_deg = 0
     grid_resolution = 200
@@ -210,55 +231,59 @@ def test_naca(return_int_pts = True):
 
     int_pts = [naca.equidistant_x[mid_res], 0.5 * (naca.equidistant_y_u[mid_res] + naca.equidistant_y_l[mid_res])]
 
-    if return_int_pts:
-        return [x],[y], [int_pts]
-    else:
-        return [x],[y]
+    return [x],[y]
 
-def demo1_xy():
+def demo1_xy(radius = 0.2, center=[-0.4,-0.2], aspect_ratio=0.7):
     theta = np.linspace(start=0, stop=2.0*np.pi, num=360)
-    x = np.cos(theta)
-    y = np.sin(theta)
-    return [x],[y],None
+    x = radius * np.cos(theta) + center[0]
+    y = radius * aspect_ratio * np.sin(theta) + center[1]
+    return [x],[y]
 
-def demo2_xy():
+def demo2_xy(center=0.3 + 1j * 0.5, outer_radius = 0.4):
     num_vertex = 5
-    inner_radius = 0.35
-    outer_vertex = np.exp(1j * np.linspace(start=0, stop=2.0 * np.pi, num=num_vertex + 1)).reshape(1,-1)
+    radius_ratio = 0.35
+    inner_radius = radius_ratio * outer_radius
+    outer_vertex = outer_radius * np.exp(1j * np.linspace(start=0, stop=2.0 * np.pi, num=num_vertex + 1)).reshape(1,-1) + center
     phase_differnce = 2.0 * np.pi / (2.0 * num_vertex)
 
     inner_vertex = inner_radius * np.exp(1j * np.linspace(start=0 - phase_differnce, stop=2.0 * np.pi - phase_differnce,
-                         num=num_vertex + 1)).reshape(1, -1)
+                         num=num_vertex + 1)).reshape(1, -1) + center
     vertex = np.concatenate((inner_vertex, outer_vertex)).T.reshape(-1)
-    return [np.real(vertex)], [np.imag(vertex)], None
+    return [np.real(vertex)], [np.imag(vertex)]
 
 
 if __name__ == '__main__':
-    """
     args = sys.argv
+
     if len(args) == 1:
-        x,y, int_pts = test_naca()
+        x,y = test_naca()
     elif args[1] == 0:
         x = []
         y = []
         for i in range(2, len(args)):
             fname = args[i]
-            x.append(np.loadtxt(fname + "x.csv", deliminater=",")
-            y.append(np.loadtxt(fname + "y.csv", deliminater=",") 
-        int_pts = None
+            x.append(np.loadtxt(fname + "x.csv", deliminater=","))
+            y.append(np.loadtxt(fname + "y.csv", deliminater=","))
+
     elif args[1] == 1:
         print("demo mode 1")
-        x, y, int_pts = demo1_xy()
+        x, y = demo1_xy()
     elif args[1] == 2:
         print("demo mode 2")
-        x, y, int_pts = demo2_xy()
-    """
-    x, y, int_pts = demo2_xy()
+        x, y = demo2_xy()
+    elif args[1] == 3:
+        print("demo mode 2")
+        x_1, y_1 = demo1_xy()
+        x_2, y_2 = demo2_xy()
+        x = [x_1[0], x_2[0]]
+        y = [y_1[0], y_2[0]]
+    else:
+        exit()
+
     size = 512
     canvas = [size,size]
-    imglim = [[-1.1,1.1],[-1.1,1.1]]
-    mask = VPmask(x,y,canvas, imglim=imglim, obj_margin=4)
-
+    #imglim = [[-1.1,1.1],[-1.1,1.1]]
+    mask = VPmask(x, y, canvas, deform="Fit", obj_margin=4, start_point="rt")
 
     # main()
 
