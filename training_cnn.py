896c24a4bae2f63422e9d03aec7106b2fbaedfc3
Mayu14 <b4123504@edu.kit.ac.jp>
2019-06-11 02:17:58 +0900

    Fix:とりあえず回せそうな状態にした

--- training_cnn.py
+++ training_cnn.py
@@ -2,10 +2,10 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Flatten, Input, Activation
+from keras.layers import Dense, Dropout, Flatten, Input, Activation, Concatenate, Lambda
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers.normalization import BatchNormalization
-from keras.layers import LeakyReLU, PReLU, Input
+from keras.layers import LeakyReLU, PReLU
 from keras.callbacks import EarlyStopping, TensorBoard
 import keras.backend.tensorflow_backend as KTF
 import tensorflow as tf
@@ -24,7 +24,13 @@ def load_data(path, size):
     
     return x_train_img, x_train_param, y_train
 
-def main():
+def main(mach=False, reynolds=False):
+    prm_num = 2
+    if mach:
+        prm_num += 1
+    if reynolds:
+        prm_num += 1
+
     source = "G:\\Toyota\\Data\\Compressible_Invicid\\training_data\\"
     npz_path = source + "NACA4\\"
     size = 512
@@ -32,20 +38,39 @@ def main():
     
     x_train_img, x_valid_img, x_train_param, x_valid_param, y_train, y_valid = train_test_split(x_train_img, x_train_param, y_train, test_size = 0.175)
 
-    inputs = Input(shape = (512, 512, 1))
-    x = Conv2D(32, kernel_size = (3, 3), input_shape = (512, 512, 1))(inputs)
+    inputs_img = Input(shape = (512, 512, 1), name="input_img")
+    inputs_prm = Input(shape = (prm_num, ), name="input_prm")
+    x = Conv2D(32, kernel_size = (3, 3), input_shape = (512, 512, 1))(inputs_img)
     x = Activation("relu")(x)
     x = Conv2D(64, (3, 3))(x)
     x = Activation("relu")(x)
     x = MaxPooling2D(pool_size = (2, 2))(x)
     x = Dropout(0.25)(x)
     x = Flatten()(x)
-    x = Dense(128)(x)
-    x = Activation("relu")(x)
     x = Dropout(0.5)(x)
-    x = Dense(10)(x)
+    x_prm = Lambda(lambda x: x, output_shape=(prm_num,))(inputs_prm)
+    x = Concatenate()([x, x_prm])
+    x = Dense(256)(x)
+    x = Activation("relu")(x)
+    x = Dense(128)(x)
     outputs = Activation("linear")(x)
-    
+
+    model = Model(inputs=[inputs_img, inputs_prm], outputs=outputs)
+
+    model.compile(loss="mean_squared_error",
+                  optimizer='Adam')
+
+
+    batch_size = 512
+    # """
+    model.fit({"input_img": x_train_img, "input_prm": x_train_param},
+              y=y_train,
+              batch_size=batch_size, epochs=50,
+              validation_split=0.1)
+
+    json_string = model.to_json()
+    open(source + "testcnn.json", 'w').write(json_string)
+    model.save_weights(source + "testweight.h5")
 
 if __name__ == '__main__':
     main()
